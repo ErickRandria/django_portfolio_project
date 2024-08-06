ec55a810e072f0bbe1b1944e50eb4aad3aed7340
Erick Randriamanantena <hajjaerick@gmail.com>
2024-08-06 19:31:52 -0400

    Adding some changes

--- portfolio/base/urls.py
+++ portfolio/base/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpattern = [
-    path('', view.home) #This is used after creatiing the home functions to render the html templates
+    path('', views.home) #This is used after creatiing the home functions to render the html templates
 ]
