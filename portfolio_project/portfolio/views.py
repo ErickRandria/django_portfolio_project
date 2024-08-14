069f9e7020b3624b346528c47eb85100ed059572
Erick Randriamanantena <hajjaerick@gmail.com>
2024-08-14 19:38:30 -0400

    change

--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -2,13 +2,39 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'portfolio/home.html')
+    projects_show = [
+        {
+            'title':'Project 1',
+            'path': 'images/projects_img/project1.png'
+        },
+        {
+            'title':'Project 2',
+            'path': 'images/projects_img/project2.png'
+        },
+        {
+            'title':'Project 3',
+            'path': 'images/projects_img/project3.png'
+        },
+        {
+            'title':'Project 4',
+            'path': 'images/projects_img/project4.png'
+        },
+        {
+            'title':'Project 5',
+            'path': 'images/projects_img/project5.png'
+        },
+        {
+            'title':'Project 6',
+            'path': 'images/projects_img/project6.png'
+        },
+    ]
+    return render(request, 'portfolio/home.html',{'projects_show': projects_show})
 
-def projects(request):
-    return render(request, 'portfolio/projects.html')
+# def projects(request):
+#     return render(request, 'portfolio/projects.html')
 
-def skills(request):
-    return render(request, 'portfolio/skills.html')
+# def skills(request):
+#     return render(request, 'portfolio/skills.html')
 
-def contact(request):
-    return render(request, 'portfolio/contact.html')
+# def contact(request):
+#     return render(request, 'portfolio/contact.html')
