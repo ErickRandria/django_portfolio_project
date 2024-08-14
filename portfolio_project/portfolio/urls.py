0e39a447816ca13cfbf87fe18fc9be8e78e48913
Erick Randriamanantena <hajjaerick@gmail.com>
2024-08-13 22:34:10 -0400

    Adding changes

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('projects/', views.projects, name='projects'),
-    path('skills/', views.skills, name='skills'),
-    path('contact/', views.contact, name='contact'),
+    path('', views.base, name='base')
+    # path('', views.home, name ='home')
+    # path('projects/', views.projects, name='projects'),
+    # path('skills/', views.skills, name='skills'),
+    # path('contact/', views.contact, name='contact'),
 ]
